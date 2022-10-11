38c6d3ec54923a7992e9d88d6e6db7c7b904ea44
Olya Bolshakova <soulful.olya@gmail.com>
2022-10-11 20:34:35 +0300

    Added a test for the ls command

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,14 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+
 public class MainTest {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
@@ -14,5 +25,18 @@ public class MainTest {
     @AfterEach
     public void restoreStreams() {
         System.setOut(originalOut);
+    }
+
+    @Test // тест команды ls: показываем все, что лежит в корне переданной файловой системы
+    public void testLS() {
+
+        String data = "ls";
+
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+
+        Bash.main(new String[]{"D:/test.zip"});
+
+        assert outContent.toString().contains("colorlib-regform-4")
+                & outContent.toString().contains("login-form-v2");
     }
 }
