f033901386d8972ff83086e0f311ea2d994c03b8
Olya Bolshakova <soulful.olya@gmail.com>
2022-10-11 14:42:07 +0300

    Added a basic setting for the test class

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,18 @@
 public class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
+    public MainTest() {
+        Locale.setDefault(new Locale("en", "US"));
+    }
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
 }
